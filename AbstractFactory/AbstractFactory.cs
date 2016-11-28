a671721b61cf11bf85599131655663d1b3d3d1ad
IllyaNovohatskyy <nachokeres@gmail.com>
2016-11-28 16:21:35 +0200

    added abstract factory
    added base pattern with abstract classes.

---
+++ AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    abstract class AbstractFactory
+    {
+        public abstract AbstractWater CreateWater();
+        public abstract AbstractBottle CreateBottle();
+    }
+}
