b96937d0c63f183da869522a4afc34dfb6c71b72
IllyaNovohatskyy <nachokeres@gmail.com>
2016-11-29 22:17:11 +0200

    added FantaFactory.cs

---
+++ AbstractFactory/FantaFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class FantaFactory : AbstractFactory
+    {
+        public override AbstractBottle CreateBottle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override AbstractCover CreateCover()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override AbstractWater CreateWater()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
