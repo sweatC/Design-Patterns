3c7010f0d103a84695dc14110c88a658383bb432
IllyaNovohatskyy <nachokeres@gmail.com>
2016-11-29 22:29:34 +0200

    added AbstractLabel.cs

---
+++ AbstractFactory/AbstractLabel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class AbstractLabel
+    {
+        public void Interact(AbstractBottle bottle)
+        {
+            Console.WriteLine(this + " interact with " + bottle);
+        }
+    }
+}
