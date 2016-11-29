b48c60512a103d0962f5ac7da8d3f80c7aac21f8
IllyaNovohatskyy <nachokeres@gmail.com>
2016-11-29 22:19:46 +0200

    added FantaCover.cs

---
+++ AbstractFactory/FantaCover.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class FantaCover : AbstractCover
+    {
+        public override void Interact(AbstractBottle bottle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
