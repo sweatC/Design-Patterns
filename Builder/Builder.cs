be8d7455a58c2c755303ffc78aac829b1f912cd4
IllyaNovohatskyy <nachokeres@gmail.com>
2016-11-30 20:20:13 +0200

    added Director.cs and Builder.cs

---
+++ Builder/Builder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    abstract class Builder
+    {
+        public abstract void BuildBottle();
+        public abstract void BuildWater();
+        public abstract void BuildCover();
+        public abstract void BuildLabel();
+    }
+}
