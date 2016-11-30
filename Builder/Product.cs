a362075f1e78eba283983aaa85a5c6a256fb6eaf
IllyaNovohatskyy <nachokeres@gmail.com>
2016-11-30 20:24:16 +0200

    added Product.cs

---
+++ Builder/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Product
+    {
+        ArrayList products = new ArrayList();
+
+        public void Add(object part)
+        {
+            products.Add(part);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Our product contains: ");
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
