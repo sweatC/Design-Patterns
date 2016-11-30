86a8b7e4122095ce41c54ded12b0c2c0e89be440
IllyaNovohatskyy <nachokeres@gmail.com>
2016-11-30 20:40:40 +0200

    added products

---
+++ Builder/CocaColaBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class CocaColaBuilder : Builder
+    {
+        Product product = new Product();
+
+        public override void BuildBottle()
+        {
+            Console.WriteLine("Bottle was built");
+        }
+
+        public override void BuildCover()
+        {
+            Console.WriteLine("Cover was built");
+        }
+
+        public override void BuildLabel()
+        {
+            Console.WriteLine("Label was built");
+        }
+
+        public override void BuildWater()
+        {
+            Console.WriteLine("Water was built");
+        }
+
+        public override Product GetResult()
+        {
+        }
+    }
+}
