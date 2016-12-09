49df2cfb5143938aa3ffb1f6bc8e592de44e4b8e
IllyaNovohatskyy <nachokeres@gmail.com>
2016-12-09 22:12:52 +0200

    added base pattern proto

---
+++ Prototype/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Prototype proto = new ConcretePrototype(5);
+                Prototype clone = proto.Clone(5);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Ooooops");
+            }
+        }
+    }
+}
---
+++ Prototype/Prototype.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    abstract class Prototype
+    {
+        public int Id { get;  private set; }
+        public Prototype(int id)
+        {
+            this.Id = id;
+        }
+
+        public abstract Prototype Clone(int id);
+        public abstract void GetId(); 
+    }
+}
