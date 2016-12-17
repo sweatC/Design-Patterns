0a26741c5d90ed6229396e1c8e55d1b050173238
IllyaNovohatskyy <1llyanovohatskyy@gmail.com>
2016-12-17 21:58:55 +0200

    added composite

---
+++ Composite/Composite.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Composite : Component
+    {
+        private ArrayList nodes = null;
+
+        public Composite(string name) : base(name)
+        {
+            nodes = new ArrayList();
+        }
+
+        public override void Add(Component c)
+        {
+            nodes.Add(c);
+        }
+
+        public override Component GetChild(int index)
+        {
+            return (Component)nodes[index];
+        }
+
+        public override void Operation()
+        {
+            Console.WriteLine(this.name);
+
+            foreach (var item in nodes)
+            {
+                (item as Component).Operation();
+            }
+        }
+
+        public override void Remove(Component c)
+        {
+            nodes.Remove(c);
+        }
+    }
+}
