c95db420bc5be59ded3843d16cefadf88659e568
IllyaNovohatskyy <1llyanovohatskyy@gmail.com>
2016-12-17 22:08:22 +0200

    added leaf and client code

---
+++ Composite/Leaf.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Leaf : Component
+    {
+        public Leaf(string name) : base(name)
+        {
+            //Because Leaf it is like Composite, but without children.
+            // It is important to understand this kind of philosophy.
+            // It is not secure, but it is clear for client.
+        }
+
+        public override void Add(Component c)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override Component GetChild(int index)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override void Operation()
+        {
+            Console.WriteLine(this.name);
+        }
+
+        public override void Remove(Component c)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
