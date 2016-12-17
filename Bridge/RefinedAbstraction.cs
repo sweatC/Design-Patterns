9cd7ad84453f920e42b1c7ea5af688468c8f6439
IllyaNovohatskyy <1llyanovohatskyy@gmail.com>
2016-12-17 20:37:47 +0200

    added pattern

---
+++ Bridge/RefinedAbstraction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class RefinedAbstraction : Abstarction
+    {
+        public RefinedAbstraction(Implementor imp)
+        {
+            this.imp = imp;
+        }
+
+        public override void Operation()
+        {
+            imp.OperationImp();
+        }
+    }
+}
