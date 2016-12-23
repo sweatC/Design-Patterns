8be9b3a3130ccdd3c0ecf73e056f7730854fcc7f
IllyaNovohatskyy <1llyanovohatskyy@gmail.com>
2016-12-23 13:06:53 +0200

    added client code

--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -10,6 +10,12 @@ namespace Flyweight
     {
         static void Main(string[] args)
         {
+            Flyweight actor = new Actor();
+            actor.Greeting("Hello, my name is Will Smith! :)");
+            Flyweight evil = new Role1(actor);
+            Flyweight kind = new Role2(actor);
+            evil.Greeting("I will destroy U!!!!!!");
+            kind.Greeting("I will defend U!!!!!!!!");
         }
     }
 }
