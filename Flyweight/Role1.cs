f3d9da7caa0da1a393ac42747b2223c38e4af065
IllyaNovohatskyy <1llyanovohatskyy@gmail.com>
2016-12-23 13:01:44 +0200

    added pattern

---
+++ Flyweight/Role1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    // Unshared
+    class Role1 : Flyweight
+    {
+        Flyweight flyweight;
+
+        public Role1(Flyweight f)
+        {
+            flyweight = f;
+        }
+
+        public override void Greeting(string speech)
+        {
+            flyweight.Greeting(speech);
+        }
+    }
+}
