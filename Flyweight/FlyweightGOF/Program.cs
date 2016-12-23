bc4ee4941b2b0f6e3c51ebdaf1a07f17569af8fd
IllyaNovohatskyy <1llyanovohatskyy@gmail.com>
2016-12-23 15:35:08 +0200

    added pattern

---
+++ Flyweight/FlyweightGOF/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightGOF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flyweight flyweight = null;
+            FlyweightFactory factory = new FlyweightFactory();
+
+            flyweight = factory.GetFlyweight("1");
+
+            flyweight.Operation(5);
+
+
+        }
+    }
+}
