b29fc97de9941f4bc0c23b24a72f78e510aac8ab
IllyaNovohatskyy <1llyanovohatskyy@gmail.com>
2016-12-25 22:15:45 +0200

    added pattern

---
+++ ChainOfResponsibility/Handler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    abstract class Handler
+    {
+        protected Handler successor = null;
+        // added END of Chain Responsibility handler
+        protected delegate void ERROR();
+        
+        public abstract void HandleRequest(int request);
+
+        public Handler SetSuccessor
+        {
+            set { successor = value; }
+        }
+
+        protected Handler GetSuccessor
+        {
+            get { return successor; }
+        }
+    }
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Handler node1 = new ConcreteHandler1();
+            Handler node2 = new ConcreteHandler2();
+
+            node1.SetSuccessor = node2;
+            node1.HandleRequest(1);
+            node1.HandleRequest(4);
+
+        }
+    }
+}
