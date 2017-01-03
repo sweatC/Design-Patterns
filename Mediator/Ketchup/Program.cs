96164d2c1516afc13525fc6af34ffe226244fee3
Ilya N <1llyanovohatskyy@gmail.com>
2017-01-03 14:43:54 +0200

    added client code

--- Mediator/Ketchup/Program.cs
+++ Mediator/Ketchup/Program.cs
@@ -10,6 +10,16 @@ namespace KetchupMediator
     {
         static void Main(string[] args)
         {
+            var mediator = new ConcreteMediator();
+            var farmer = new Farmer(mediator);
+            var cannery = new Cannery(mediator);
+            var shop = new Shop(mediator);
+
+            mediator.Farmer = farmer;
+            mediator.Cannery = cannery;
+            mediator.Shop = shop;
+
+            farmer.GrowTomato();
         }
     }
 }
