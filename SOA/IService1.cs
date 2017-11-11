b7cc5363b32e29be87e4a865b8b9ac8f5f26cab1
Ilya N <1llyanovohatskyy@gmail.com>
2017-11-11 22:43:06 +0200

    added wcf rest architecture

---
+++ SOA/IService1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SystemActivityMonitorService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+        #region Common Methods
+
+        /// <summary>
+        /// проверка соединения
+        /// </summary>
+        /// <returns> OK </returns>
+        [OperationContract]
+        [WebInvoke(Method = "GET")]
+        string TestConnection();
+
+        /// <summary>
+        /// получить все процессы локалки
+        /// </summary>
+        /// <returns> Dictionary<int, string> </returns>
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "processes")]
+        Dictionary<int, string> GetProcesses();
+
+        /// <summary>
+        /// получить процесс локалки по имени
+        /// </summary>
+        /// <returns> Dictionary<int, string> </returns>
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "processes/{name}")]
+        Dictionary<int, string> GetProcessByName(string name);
+        #endregion
+
+        #region Arithmetic
+
+        /// <summary>
+        /// сложение
+        /// </summary>
+        /// <param name="a"> слагаемое 1 </param>
+        /// <param name="b"> слагаемое 2 </param>
+        /// <returns> сумма </returns>
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "Add?a={a}&b={b}")]
+        double Addition(double a, double b);
+
+        /// <summary>
+        /// вычитание
+        /// </summary>
+        /// <param name="a"> уменьшаемое </param>
+        /// <param name="b"> вычитаемое </param>
+        /// <returns> разность </returns>
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "Sub?a={a}&b={b}")]
+        double Subtraction(double a, double b);
+
+        /// <summary>
+        /// умножение
+        /// </summary>
+        /// <param name="a"> множитель 1 </param>
+        /// <param name="b"> множитель 2 </param>
+        /// <returns> произведение </returns>
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "Mul?a={a}&b={b}")]
+        double Multiplication(double a, double b);
+
+        /// <summary>
+        /// деление
+        /// </summary>
+        /// <param name="a"> делимое </param>
+        /// <param name="b"> делитель </param>
+        /// <returns> частное </returns>
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "Div?a={a}&b={b}")]
+        double Division(double a, double b);
+
+        #endregion
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ SOA/Service1.svc.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Diagnostics;
+
+namespace SystemActivityMonitorService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+        #region Common Methods
+
+        /// <summary>
+        /// проверка соединения
+        /// </summary>
+        /// <returns> OK </returns>
+        public string TestConnection()
+        {
+            return "OK";
+        }
+        /// <summary>
+        /// получить все процессы локалки
+        /// </summary>
+        /// <returns> [] of Processes </returns>
+        public Dictionary<int, string> GetProcesses()
+        {
+            Dictionary<int, string> list = new Dictionary<int, string>();
+
+            int i = 0;
+            foreach (Process process in Process.GetProcesses())
+            {
+                list[i] = "Process id: " + process.Id + ", Process Name: " + process.ProcessName;
+                i++;
+            }
+
+            return list;
+        }
+
+        public Dictionary<int, string> GetProcessByName(string name)
+        {
+            Dictionary<int, string> list = new Dictionary<int, string>();
+
+            int i = 0;
+            foreach (Process process in Process.GetProcessesByName(name))
+            {
+                list[i] = "Process id: " + process.Id + ", Process Name: " + process.ProcessName;
+                i++;
+            }
+
+            return list;
+        }
+
+        #endregion
+
+        #region Arithmetic
+
+        /// <summary>
+        /// сложение
+        /// </summary>
+        /// <param name="a"> слагаемое 1 </param>
+        /// <param name="b"> слагаемое 2 </param>
+        /// <returns> сумма </returns>
+        public double Addition(double a, double b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// вычитание
+        /// </summary>
+        /// <param name="a"> уменьшаемое </param>
+        /// <param name="b"> вычитаемое </param>
+        /// <returns> разность </returns>
+        public double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// умножение
+        /// </summary>
+        /// <param name="a"> множитель 1 </param>
+        /// <param name="b"> множитель 2 </param>
+        /// <returns> произведение </returns>
+        public double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// деление
+        /// </summary>
+        /// <param name="a"> делимое </param>
+        /// <param name="b"> делитель </param>
+        /// <returns> частное </returns>
+        public double Division(double a, double b)
+        {
+            return a / b;
+        }
+
+        #endregion
+    }
+}
